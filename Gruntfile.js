6bf36c1f67a808aed768689c8f34f5a829724c37
dcdrawk <dcdrawk@gmail.com>
2015-08-20 20:26:56 -0700

    new build

--- Gruntfile.js
+++ Gruntfile.js
@@ -324,7 +324,7 @@ module.exports = function (grunt) {
           usemin: 'scripts/scripts.js'
         },
         cwd: '<%= yeoman.app %>',
-        src: 'views/**/*.html',
+        src: 'views/templates/**/*.html',
         dest: '.tmp/templateCache.js'
       }
     },
