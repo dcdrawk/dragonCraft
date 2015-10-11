808297a1be1ad93ff1f3a3dc36e2e903dd9dfdb0
devin <devin@thewebdev.ca>
2015-10-11 16:54:10 -0700

    change character creation to use db

---
+++ app/scripts/controllers/databaseAdminCtrl.js
@@ -0,0 +1,20 @@
+'use strict';
+angular.module('dCraftApp').controller('databaseAdminCtrl', function($scope, $indexedDB, $ocLazyLoad, $injector){
+  
+  $ocLazyLoad.load('/scripts/services/databaseAdminSrv.js').then(function(e){
+    var databaseAdminSrv = $injector.get('databaseAdminSrv');
+    databaseAdminSrv.checkMetadata();
+    
+    $scope.deleteDatabase = function(){
+      databaseAdminSrv.deleteDatabase();
+    };
+    
+    $scope.repopulateDatabase = function(){
+      databaseAdminSrv.checkMetadata();
+    };
+    
+    $scope.clearDatabase = function(){
+      databaseAdminSrv.clearData();
+    };
+  });
+});
