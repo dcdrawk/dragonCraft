fe206e809e708bc6c40ddc787830dec0cb6cdd92
devin <devin@thewebdev.ca>
2015-10-13 00:46:46 -0700

    added missing files

---
+++ dist/scripts/services/databaseSrv.js
@@ -0,0 +1,136 @@
+'use strict';
+
+angular.module('dCraftApp').service('databaseSrv', function($indexedDB, $q){
+  var self = this;
+  
+  //Alignments
+  this.getAlignments = function(){
+    var deferred = $q.defer();
+    $indexedDB.openStore('alignments', function(alignmentsStore){
+      alignmentsStore.getAll().then(function(e){
+        self.alignments = e;
+        deferred.resolve(self.alignments);
+      });
+    });
+    return deferred.promise;    
+  };
+  
+  //Backgrounds
+  this.getBackgrounds = function(){
+    var deferred = $q.defer();
+    $indexedDB.openStore('backgrounds', function(store){
+      store.getAll().then(function(e){
+        self.backgrounds = e;
+        deferred.resolve(self.backgrounds);
+      });
+    });
+    return deferred.promise;    
+  };
+  
+  
+  
+  //Class Names
+  this.getClassNames = function(){
+    var deferred = $q.defer();
+    $indexedDB.openStore('classes', function(classesStore){
+      classesStore.getAllKeys().then(function(e){
+        self.classNames = e;
+        deferred.resolve(self.classNames);
+      });
+    });
+    //$indexedDB.closeDatabase();
+    return deferred.promise;    
+  };
+  
+  //Class Info (hit points, proficiency);
+  this.getClassInfo = function(className){
+    var deferred = $q.defer();
+    
+    $indexedDB.openStore('classes', function(classesStore){
+      var find = classesStore.query();
+      find = find.$eq(className);
+      classesStore.eachWhere(find).then(function(e){
+        self.classNames = e;
+        deferred.resolve(self.classNames);
+      });
+    });
+    //$indexedDB.closeDatabase();
+    return deferred.promise;    
+  };
+  
+  //Race Names
+  this.getRaceNames = function(){
+    var deferred = $q.defer();
+    $indexedDB.openStore('races', function(racesStore){
+      racesStore.getAllKeys().then(function(e){
+        self.raceNames = e;
+        deferred.resolve(self.raceNames);
+      });
+    });
+    //$indexedDB.closeDatabase();
+    return deferred.promise;    
+  };
+  
+  //Race Traits
+  this.getRaceTraits = function(race){    
+    var deferred = $q.defer();
+    $indexedDB.openStore('races', function(racesStore){      
+      var find = racesStore.query();
+      find = find.$eq(race);
+      racesStore.eachWhere(find).then(function(e){
+        self.raceInfo = e;
+        deferred.resolve(self.raceInfo);
+      });
+    });
+    return deferred.promise;    
+  };
+  
+  //Subrace Names
+  this.getSubraceNames = function(race){
+    var deferred = $q.defer();
+    $indexedDB.openStore('subraces', function(subracesStore){
+      
+      var find = subracesStore.query();
+      find = find.$eq(race);
+      find = find.$index('race_idx');    
+      
+      subracesStore.eachWhere(find).then(function(e){
+        console.log(e);
+        self.subraceNames = e;
+        deferred.resolve(self.subraceNames);
+      });
+    });
+    //$indexedDB.closeDatabase();
+    return deferred.promise;    
+  };
+  
+  //Subrace Traits
+  this.getSubraceTraits = function(subrace){    
+    var deferred = $q.defer();
+    $indexedDB.openStore('subraces', function(store){      
+      var find = store.query();
+      find = find.$eq(subrace);
+      store.eachWhere(find).then(function(e){
+        self.subraceInfo = e;
+        deferred.resolve(self.subraceInfo);
+      });
+    });
+    return deferred.promise;    
+  };
+  
+  
+  //Feats
+  this.getFeats = function(subrace){
+    var deferred = $q.defer();
+    $indexedDB.openStore('feats', function(store){     
+    store.getAll().then(function(e){
+      console.log(e)
+        self.feats = e;
+        deferred.resolve(self.feats);
+      });
+    });    
+    return deferred.promise;
+  };
+  
+  
+});
---
+++ dist/scripts/services/saveCharacterSrv.js
@@ -0,0 +1,52 @@
+'use strict';
+
+angular.module('dCraftApp').service('saveCharacterSrv', function(){
+  
+  var storage = localStorage;
+  var newArray = [];
+  var lastID = '0';
+  
+  
+  //Check to see if save data exists
+  if(!storage.getObj('characters')){
+    console.log('No save data exists, setting up...');
+    storage.setObj('characters', []);
+  }
+  
+  //Add New Character
+  this.addNewCharacter = function(character){
+    var characters = storage.getObj('characters');
+    //Add a unique id to the character...
+    if(characters.length === 0){
+      character.id = 0;
+    } else {
+      for(var i in characters){
+        if(characters[i].id > lastID){
+          lastID = characters[i].id;          
+        }
+      }
+      character.id = parseInt(lastID) + 1;
+    }
+    characters.push(character);    
+    storage.setObj('characters', characters);
+  };
+  
+  //Delete All Characters
+  this.deleteAllCharacters = function(){
+    storage.setObj('characters', []);
+  };
+  
+  //Update Character
+  this.updateCharacter = function(id, field, value){    
+    var characters = storage.getObj('characters');
+    for(var i in characters){
+      if(id === parseInt(characters[i].id)){
+        characters[i][field] = value;
+        storage.setObj('selectedCharacter', characters[i]);
+        characters = storage.setObj('characters', characters);
+        return;
+      }
+    }
+  };
+  
+});
