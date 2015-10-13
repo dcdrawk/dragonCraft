a46cb740c520f30c845a9280771b7d1645536c46
devin <devin@thewebdev.ca>
2015-10-13 00:40:15 -0700

    checks database on root page load

--- app/scripts/controllers/characterListCtrl.js
+++ app/scripts/controllers/characterListCtrl.js
@@ -1,9 +1,14 @@
 'use strict';
-angular.module('dCraftApp').controller('characterListCtrl', function($scope, characterSrv){
+angular.module('dCraftApp').controller('characterListCtrl', function($scope, characterSrv, $ocLazyLoad, $injector){
   $scope.characterList = characterSrv.getCharacterList();
   
   $scope.selectCharacter = function(id){
     characterSrv.selectCharacter(id);
   };
+  
+  $ocLazyLoad.load('/scripts/services/databaseAdminSrv.js').then(function(e){
+    var databaseSrv = $injector.get('databaseAdminSrv');
+    databaseSrv.checkMetadata();
+  });
   
 });
